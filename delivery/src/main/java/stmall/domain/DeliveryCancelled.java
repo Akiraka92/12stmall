637a72e42b9241b9885f079baaae36b750ccc0db
Akiraka92 <37332739+Akiraka92@users.noreply.github.com>
2023-05-09 09:36:11 +0900

    commit

---
+++ delivery/src/main/java/stmall/domain/DeliveryCancelled.java
@@ -0,0 +1,26 @@
+package stmall.domain;
+
+import java.util.*;
+import lombok.*;
+import stmall.domain.*;
+import stmall.infra.AbstractEvent;
+
+@Data
+@ToString
+public class DeliveryCancelled extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private Long productId;
+    private Integer qty;
+    private String status;
+    private String productName;
+
+    public DeliveryCancelled(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryCancelled() {
+        super();
+    }
+}
---
+++ order/src/main/java/stmall/domain/OrderCancelled.java
@@ -0,0 +1,25 @@
+package stmall.domain;
+
+import java.util.*;
+import lombok.*;
+import stmall.domain.*;
+import stmall.infra.AbstractEvent;
+
+@Data
+@ToString
+public class OrderCancelled extends AbstractEvent {
+
+    private Long id;
+    private Long productId;
+    private Long userId;
+    private Integer qty;
+    private String productName;
+
+    public OrderCancelled(Order aggregate) {
+        super(aggregate);
+    }
+
+    public OrderCancelled() {
+        super();
+    }
+}
